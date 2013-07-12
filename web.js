febb0d74045e2ff412939bad848a542900ef5363
Thibaud Colas <thibaudcolas@gmail.com>
2013-07-12 04:42:51 +0000

    Add index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var html = fs.readFileSync('index.html', 'utf8');
+  response.send(html);
 });
 
 var port = process.env.PORT || 5000;
